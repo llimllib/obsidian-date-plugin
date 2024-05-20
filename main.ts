75e749b19435a7a9c3384645dff906178063b141
Bill Mill <bill@billmill.org>
2024-05-20 09:19:48 -0400

    feat: add created and updated dates on new note creation

--- main.ts
+++ main.ts
@@ -1,15 +1,37 @@
-import { App, Plugin, Editor, MarkdownView } from "obsidian";
+import {
+    App,
+    Plugin,
+    Editor,
+    MarkdownView,
+    TAbstractFile,
+    TFile,
+} from "obsidian";
 
-async function addDateToOneNote(
+function addDatesToNewFile(app: App): (file: TAbstractFile, ctx?: any) => any {
+    return (file: TAbstractFile, _ctx?: any) => {
+        if (!("stat" in file)) {
+            return;
+        }
+        app.fileManager.processFrontMatter(
+            file as TFile,
+            (data: Record<string, any>) => {
+                data["created"] = new Date();
+                data["updated"] = new Date();
+            },
+        );
+    };
+}
+
+function addDateToOneNote(
     app: App,
-): Promise<(_editor: Editor, ctx: MarkdownView) => void> {
+): (_editor: Editor, ctx: MarkdownView) => void {
     return (_editor: Editor, ctx: MarkdownView) => {
         if (!ctx.file) {
+            console.warn("unexpected missing file");
             return;
         }
         app.fileManager.processFrontMatter(
             ctx.file,
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
             (data: Record<string, any>) => {
                 data["updated"] = new Date();
             },
@@ -19,10 +41,21 @@ async function addDateToOneNote(
 
 export default class IDPlugin extends Plugin {
     async onload() {
+        // Wait for the vault to be ready, so that we don't get called on the
+        // initial load of every file:
+        //
+        // > If you do not wish to receive create events on vault load,
+        // register your event handler inside Workspace.onLayoutReady.
+        this.app.workspace.onLayoutReady(async () => {
+            this.registerEvent(
+                this.app.vault.on("create", addDatesToNewFile(this.app)),
+            );
+        });
+
         this.addCommand({
             id: "update-this-note",
             name: "Update the updated date for this note",
-            editorCallback: await addDateToOneNote(this.app),
+            editorCallback: addDateToOneNote(this.app),
         });
     }
 }
